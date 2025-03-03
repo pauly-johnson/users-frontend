2d4ad094fa1c7ea4997597d732600926e27570b0
pauly-johnson <paul.m.johnson1988@gmail.com>
2025-03-04 12:01:09 +1300

    updated register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,72 +1,82 @@
-import React, { useState } from 'react';
-import { registerUser } from '../api';
+import React, { useState } from "react";
+import { registerUser } from "../api";
 
 const Register = ({ onRegisterSuccess, onRegisterClick }) => {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (password !== confirmPassword) {
-            alert('Passwords do not match.');
-            return;
-        }
-        setIsLoading(true);
-        onRegisterClick(); // Hide the "Back to Login" button
-        try {
-            await registerUser(username, email, password);
-            alert('Registration successful!');
-            onRegisterSuccess(username, password);
-        } catch (err) {
-            alert('Registration failed.');
-            setIsLoading(false);
-            onRegisterClick(true); // Show the "Back to Login" button again if registration fails
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords do not match.");
+      return;
+    }
+    setIsLoading(true);
+    onRegisterClick(); // Hide the "Back to Login" button
+    try {
+      await registerUser(username, email, password);
+      alert("Registration successful!");
+      setError("");
+      onRegisterSuccess(username, password);
+    } catch (err) {
+      if (err.response && err.response.status === 400) {
+        setError("Username taken");
+      } else {
+        setError("Registration failed.");
+        alert("Registration failed.");
+      }
+      setIsLoading(false);
+      onRegisterClick(true); // Show the "Back to Login" button again if registration fails
+    }
+  };
 
-    return (
-        <div>
-            {isLoading ? (
-                <p>Registering Please Wait...</p>
-            ) : (
-                <form onSubmit={handleSubmit}>
-                    <h2>Register</h2>
-                    <input 
-                        type="text" 
-                        placeholder="Username" 
-                        value={username} 
-                        onChange={(e) => setUsername(e.target.value)} 
-                        required 
-                    />
-                    <input 
-                        type="email" 
-                        placeholder="Email" 
-                        value={email} 
-                        onChange={(e) => setEmail(e.target.value)} 
-                        required 
-                    />
-                    <input 
-                        type="password" 
-                        placeholder="Password" 
-                        value={password} 
-                        onChange={(e) => setPassword(e.target.value)} 
-                        required 
-                    />
-                    <input 
-                        type="password" 
-                        placeholder="Confirm Password" 
-                        value={confirmPassword} 
-                        onChange={(e) => setConfirmPassword(e.target.value)} 
-                        required 
-                    />
-                    <button type="submit">Register</button>
-                </form>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      {isLoading ? (
+        <p>Registering Please Wait...</p>
+      ) : (
+        <>
+          <form onSubmit={handleSubmit}>
+            <h2>Register</h2>
+            <p style={{ color: "red" }}>{error}</p>
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+            <button type="submit">Register</button>
+          </form>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Register;
